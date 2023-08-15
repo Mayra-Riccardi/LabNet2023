ffac97affd6e97a22a955fb588326d3bbe393d6a
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-08-15 20:49:53 -0300

    Adding code for HelloWorld Adding code, proyect and solution

---
+++ LabNet2023-HelloWorld/LabNet2023-HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LabNet2023_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           
+        }
+    }
+}
