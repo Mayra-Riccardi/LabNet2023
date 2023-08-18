d7aa957c718bd6f14ffeaeda90aad522c1fe4de8
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-08-18 20:00:26 -0300

    Agregamos clases, metodos, variables Agregamos manejo de excepciones

---
+++ Practica1/Practica1/Omnibus.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica01
+{
+    internal class Omnibus : TransportePublico
+    {
+        public Omnibus(int cantPasajeros) : base(cantPasajeros)
+        {
+
+        }
+
+        public override string Avanzar()
+        {
+            return string.Format($"avanza muy lento");
+        }
+
+        public override string Detenerse()
+        {
+            return string.Format($"Se detiene abruptamente");
+        }
+    }
+}
---
+++ Practica1/Practica1/TransportePublico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica01
+{
+    public abstract class TransportePublico
+    {
+
+        internal TransportePublico(int cantPasajeros) //constructor de tipo entero, lo primero que se ejecuta en la instacia de la clase, ahora lo paso por parametro
+        {
+            Pasajeros = cantPasajeros;//le damos el valor a traves del constructor, pasa por el constructor y se la da a la propiedad un valor
+        }
+
+        internal int Pasajeros { get; set; }//propiedad de tipo entera, publica
+
+        public abstract string Avanzar();
+
+        public abstract string Detenerse();
+
+    }
+}
