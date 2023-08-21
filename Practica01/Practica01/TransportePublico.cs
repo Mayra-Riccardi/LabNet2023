f02b76632ddd38ccc338fcd216482cd4f80cf41e
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-08-21 19:36:33 -0300

    Correcciones

--- Practica01/Practica01/TransportePublico.cs
+++ Practica01/Practica01/TransportePublico.cs
@@ -11,10 +11,11 @@ namespace Practica01
 
         public TransportePublico(int cantPasajeros) //constructor de tipo entero, lo primero que se ejecuta en la instacia de la clase, ahora lo paso por parametro
         {
-            Pasajeros = cantPasajeros;//le damos el valor a traves del constructor, pasa por el constructor y se la da a la propiedad un valor
+            pasajeros = cantPasajeros;//le damos el valor a traves del constructor, pasa por el constructor y se la da a la propiedad un valor
         }
 
-        protected internal int Pasajeros { get; set; }//propiedad de tipo entera privada, o protegida
+        private int pasajeros; //defino variable privada pasajeros
+        public int Pasajeros { get { return pasajeros; } set { pasajeros = value; } }//propiedad pública llamada Pasajeros, get y set para obtener valores de pasajeros y para asignar un valor
 
         public abstract string Avanzar();
 
