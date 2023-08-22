b4bd696421653d5d65fe77b0bea72d269b5d640f
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-08-21 22:15:14 -0300

    Creacion de los metodos DividirPorCero y Divison

---
+++ practica2/practica2/Calculadora.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica2
+{
+    public class Calculadora
+    {
+        public static void DivisionPorCero(int numero, int divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("Caiste en la trampa, nadie te dijo nunca que no se puede dividir por cero? Volve a intentar otro dia!");
+            }
+        }
+
+        public static string Division(int dividendo, int divisor)
+        {
+            try
+            {
+                int resultado = dividendo / divisor;
+                return $"El resultado de la division es: {resultado}\n\n\nMuchas Gracias por usar nuestra calculadora!!!!";
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new DivideByZeroException ($"¡Solo Chuck Norris divide por cero!\nMensaje propio de la excepcion: {ex.Message}");
+            }
+            catch (FormatException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- practica2/practica2/Program.cs
+++ practica2/practica2/Program.cs
@@ -10,6 +10,74 @@ namespace practica2
     {
         static void Main(string[] args)
         {
+            bool entradaValida = false;
+            int eleccion = 0;
+
+            while (!entradaValida)
+            {
+                Console.WriteLine("Elija una operacion a realizar:");
+                Console.WriteLine("1. Division por cero");
+                Console.WriteLine("2. Division de dos numeros");
+
+                string entrada = Console.ReadLine();
+
+                if (int.TryParse(entrada, out eleccion) && (eleccion == 1 || eleccion == 2))
+                {
+                    entradaValida = true;
+                }
+                else
+                {
+                    Console.WriteLine("El valor que has ingresado no es valido, recorda que debes ingresar 1 o 2. Intente nuevamente.");
+                }
+            }
+
+            switch (eleccion)
+            {
+                case 1:
+                    try
+                    {
+                        Console.WriteLine("Ingresa un numero para realizar la division por 0: ");
+                        int numero = int.Parse(Console.ReadLine());
+
+                        Calculadora.DivisionPorCero(numero, 0);
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine($"Hubo una excepcion: {ex.Message}");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Operacion finalizada!");
+                    }
+                    break;
+                case 2:
+                    try
+                    {
+                        Console.WriteLine("Ingresa el dividendo: ");
+                        int dividendo = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Ingresa el divisor: ");
+                        int divisor = int.Parse(Console.ReadLine());
+
+                        string resultado = Calculadora.Division(dividendo, divisor);
+                        Console.WriteLine(resultado);
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine($"Hubo un error: {ex.Message}");
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine($"Seguro ingreso una letra o no ingreso nada!\nMensaje propio de la excepcion: {ex.Message}");
+                    }
+                    finally
+                    {
+                        Console.WriteLine("Operacion finalizada!");
+                    }
+                    break;
+            }
+
+            Console.ReadLine();
         }
     }
 }
