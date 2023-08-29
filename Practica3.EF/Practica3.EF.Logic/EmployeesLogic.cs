ad0a0d85477de2056000b02a0c5265f218978d3b
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-08-29 04:30:32 -0300

    Practica 3 Conexion a la Base de Datos GetAll y GetById con Employees

---
+++ Practica3.EF/Practica3.EF.Logic/EmployeesLogic.cs
@@ -0,0 +1,32 @@
+﻿using Practica3.EF.Data;
+using Practica3.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3.EF.Logic
+{
+    public class EmployeesLogic
+    {
+        private NorthwindContext _context;
+
+        public EmployeesLogic()
+        {
+            _context = new NorthwindContext();
+        }
+
+
+        public List<Employees> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employees GetById(int employeeId)
+        {
+            return _context.Employees.FirstOrDefault(e => e.EmployeeID == employeeId);
+        }
+
+    }
+}
---
+++ Practica3.EF/Practica3.EF/Program.cs
@@ -0,0 +1,48 @@
+﻿using Practica3.EF.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica3.EF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeesLogic employeesLogic = new EmployeesLogic();
+
+            var employees = employeesLogic.GetAll();
+
+            Console.WriteLine("Employees List:");
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"ID: {employee.EmployeeID} - NOMBRE Y APELLIDO: {employee.LastName} {employee.FirstName} - PAIS: {employee.Country}");
+            }
+
+            Console.WriteLine("Enter an employee ID to get details:");
+            if (int.TryParse(Console.ReadLine(), out int employeeId))
+            {
+                var employee = employeesLogic.GetById(employeeId);
+                if (employee != null)
+                {
+                    Console.WriteLine($"Detalles del empleado con ID {employee.EmployeeID}:");
+                    Console.WriteLine($"Nombre completo: {employee.LastName} {employee.FirstName}");
+                    Console.WriteLine($"País: {employee.Country}");
+                }
+                else
+                {
+                    Console.WriteLine("No se encontró ningún empleado con el ID proporcionado.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida de ID de empleado.");
+            }
+            Console.ReadLine();
+        }
+        
+    }
+}
