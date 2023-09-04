e8c1a61dfadbb12d49b02cd6ec1c4b84e3b217e5
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-09-03 21:27:34 -0300

    Configuracion Entity Framework Conexion a DB Northwind

---
+++ Practica4.LQ/Practica4.LQ.Logic/CustomersLogic.cs
@@ -0,0 +1,21 @@
+﻿using Practica4.LQ.Data;
+using Practica4.LQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4.LQ.Logic
+{
+    public class CustomersLogic : BaseLogic
+    {
+
+        public CustomersLogic() : base() { }
+
+        public List<Customers> GetAll()
+        {
+           return _context.Customers.ToList();
+        }
+    }
+}
---
+++ Practica4.LQ/Practica4.LQ/Program.cs
@@ -0,0 +1,25 @@
+﻿using Practica4.LQ.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4.LQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomersLogic logic = new CustomersLogic();
+
+            var customers = logic.GetAll();
+
+            foreach ( var customer in customers ) 
+            {
+                Console.WriteLine(customer.ContactName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
