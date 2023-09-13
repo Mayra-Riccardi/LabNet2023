3e4053ae5f9f585e90ee3dd910c451051864fea1
Mayra-Riccardi <may_ricca5@hotmail.com>
2023-09-13 03:15:35 -0300

    Cosumiendo Api JSONPlaceholder Modelo, Controler y Vista creados

---
+++ Practica3.EF/Practica5.MVC/Controllers/ApiController.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Practica5.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Mvc;
+
+namespace Practica5.MVC.Controllers
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            try
+            {
+                var httpClient = new HttpClient();
+                var json = httpClient.GetStringAsync("https://jsonplaceholder.typicode.com/users").Result;
+                var users = JsonConvert.DeserializeObject<List<ApiUserView>>(json);
+
+                return View(users);
+            }
+            catch (Exception ex)
+            {
+                return View(new List<ApiUserView>());
+            }
+        }
+    }
+}
